ea3ef53d285d75716c704bf58ef7f419cdf1cb88
kanysaiorozbekkyzy <k.orozbekkyzy0707@gmail.com>
2022-04-13 20:31:16 -0500

    Added hasCar and carModel. Deleted salary.

--- src/test/java/steps/Ruslan.java
+++ src/test/java/steps/Ruslan.java
@@ -4,6 +4,7 @@ public class Ruslan {
     public static void main(String[] args) {
         String name="Ruslan";
         int age=37;
-        double salary=120.00;
+        boolean hasCar=true;
+        String carModal="Camry";
     }
 }
